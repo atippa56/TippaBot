840b3dbc86015fe9b3f31c5d2116ccf6c5bba39d
aarushtippa <aarushtippa@users.noreply.github.com>
2025-07-23 16:21:53 -0400

    Fix NameError: add missing 'from threading import Thread' import for background cache thread

--- trading_bot.py
+++ trading_bot.py
@@ -34,6 +34,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.exc import OperationalError
+from threading import Thread
 
 # Configure logging
 logging.basicConfig(
